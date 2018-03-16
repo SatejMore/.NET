78e31a4f4a76fb4fe10282d4c84edecb7a981b2c
Satej More <satejmore@gmail.com>
2018-03-16 22:37:44 +0530

    -Added code related to Single Responsibility Principle.

---
+++ DesignPatterns/SOLIDPrinciples/OCP/ClsProducts.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrinciples.OCP
+{
+    public enum Color { RED, GREEN, BLUE}
+
+    public enum Size { SMALL, MEDIUM, LARGE, HUGE}
+
+    internal class ClsProducts
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public ClsProducts(string name, Color color, Size size)
+        {
+            Name = name;
+            Color = color;
+            Size = size;
+        }//end of ctor
+            
+    }//end of class ClsProducts
+
+    internal class ProductFilter
+    {
+        internal IEnumerable<ClsProducts> FilterByColor(IEnumerable<ClsProducts> products, Color color)
+        {
+            foreach (var product in products)
+            {
+                if (product.Color == color)
+                    yield return product;
+            }//end of foreach
+        }//end of function FilterByColor
+
+        internal IEnumerable<ClsProducts> FilterBySize(IEnumerable<ClsProducts> products, Size size)
+        {
+            foreach (var product in products)
+            {
+                if (product.Size == size)
+                    yield return product;
+            }//end of foreach
+        }//end of function FilterBySize
+
+        internal IEnumerable<ClsProducts> FilterByColorAndSize(IEnumerable<ClsProducts> products, Color color, Size size)
+        {
+            foreach (var product in products)
+            {
+                if (product.Color == color && product.Size == size)
+                    yield return product;
+            }//end of foreach
+        }//end of function FilterByColorAndSize
+
+    }//end of class ProductFilter
+
+}//end of namespace SOLIDPrinciples.OCP
---
+++ DesignPatterns/SOLIDPrinciples/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using SOLIDPrinciples.SRP;
+using SOLIDPrinciples.OCP;
+
+namespace SOLIDPrinciples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region SRP
+            //ClsJournal objJournal = new ClsJournal();
+            //objJournal.AddEntry("Hello Everyone");
+            //objJournal.AddEntry("How are you?");
+            //Console.WriteLine(objJournal.ToString());
+
+            //ClsPersistence objPersistence = new ClsPersistence();
+            //var fileName = "journal.txt";
+            //objPersistence.SaveToFile(objJournal, fileName);
+            //Process.Start(fileName);
+            #endregion
+
+            #region OCP
+            ClsProducts objProduct1 = new ClsProducts("Apple", Color.RED, Size.MEDIUM);
+            ClsProducts objProduct2 = new ClsProducts("Watermelon", Color.GREEN, Size.MEDIUM);
+            ClsProducts objProduct3 = new ClsProducts("Guvava", Color.GREEN, Size.MEDIUM);
+
+            ClsProducts[] productList = { objProduct1, objProduct2, objProduct3 };
+
+            ProductFilter objFilter = new ProductFilter();
+            Console.WriteLine("Green Products (Old):");
+            foreach(var product in objFilter.FilterByColor(productList, Color.GREEN))
+            {
+                Console.WriteLine($"{product.Name} is Green");
+            } 
+            #endregion
+        }
+    }//end of class Program
+}//end of namespace SOLIDPrinciples

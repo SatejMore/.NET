90c511080fecc7f24b057d4fa4faa305b6078005
Satej More <satejmore@gmail.com>
2018-03-17 13:23:36 +0530

    -Added example of Liskov Substitution principle.

---
+++ DesignPatterns/SOLIDPrinciples/LSP/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrinciples.LSP
+{
+    internal class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}:{Width},{nameof(Height)}:{Height}";
+        }
+    }//end of class Rectangle
+
+    internal class Square : Rectangle
+    {
+        //public new int Width
+        //{
+        //    set { base.Width = base.Height = value; }
+        //}
+
+        //public new int Height
+        //{
+        //    set { base.Height = base.Width = value; }
+        //}
+
+        public override int Width
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public override int Height
+        {
+            set { base.Height = base.Width = value; }
+        }
+    }
+}//end of namespace SOLIDPrinciples.LSP
--- DesignPatterns/SOLIDPrinciples/Program.cs
+++ DesignPatterns/SOLIDPrinciples/Program.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using SOLIDPrinciples.SRP;
 using SOLIDPrinciples.OCP;
+using SOLIDPrinciples.LSP;
 
 namespace SOLIDPrinciples
 {
     class Program
     {
+        static public int GetArea(Rectangle rec) => rec.Width* rec.Height;
+
         static void Main(string[] args)
         {
             #region SRP
@@ -26,42 +29,51 @@ namespace SOLIDPrinciples
             #endregion
 
             #region OCP
-            ClsProducts objProduct1 = new ClsProducts("Apple", Color.RED, Size.SMALL);
-            ClsProducts objProduct2 = new ClsProducts("Watermelon", Color.GREEN, Size.MEDIUM);
-            ClsProducts objProduct3 = new ClsProducts("Guvava", Color.GREEN, Size.MEDIUM);
+            //ClsProducts objProduct1 = new ClsProducts("Apple", Color.RED, Size.SMALL);
+            //ClsProducts objProduct2 = new ClsProducts("Watermelon", Color.GREEN, Size.MEDIUM);
+            //ClsProducts objProduct3 = new ClsProducts("Guvava", Color.GREEN, Size.MEDIUM);
 
-            ClsProducts[] productList = { objProduct1, objProduct2, objProduct3 };
+            //ClsProducts[] productList = { objProduct1, objProduct2, objProduct3 };
 
-            ProductFilter objFilter = new ProductFilter();
-            Console.WriteLine("Green Products (Old):");
-            foreach(var product in objFilter.FilterByColor(productList, Color.GREEN))
-            {
-                Console.WriteLine($"{product.Name} is Green");
-            }
+            //ProductFilter objFilter = new ProductFilter();
+            //Console.WriteLine("Green Products (Old):");
+            //foreach(var product in objFilter.FilterByColor(productList, Color.GREEN))
+            //{
+            //    Console.WriteLine($"{product.Name} is Green");
+            //}
 
-            Console.WriteLine("*****************After*******************");
+            //Console.WriteLine("*****************After*******************");
 
-            BetterFilter bf = new BetterFilter();
-            Console.WriteLine("Green Products");
-            foreach (var prod in bf.Filter(productList,new ColorSpecification(Color.GREEN)))
-            {
-                Console.WriteLine($"{prod.Name} is Green");
-            }//end of for each
+            //BetterFilter bf = new BetterFilter();
+            //Console.WriteLine("Green Products");
+            //foreach (var prod in bf.Filter(productList,new ColorSpecification(Color.GREEN)))
+            //{
+            //    Console.WriteLine($"{prod.Name} is Green");
+            //}//end of for each
 
 
-            Console.WriteLine("Small size products");
-            foreach (var prod in bf.Filter(productList, new SizeSpecification(Size.SMALL)))
-            {
-                Console.WriteLine($"{prod.Name} is small in size");
-            }//end of foreach
+            //Console.WriteLine("Small size products");
+            //foreach (var prod in bf.Filter(productList, new SizeSpecification(Size.SMALL)))
+            //{
+            //    Console.WriteLine($"{prod.Name} is small in size");
+            //}//end of foreach
 
-            Console.WriteLine("Green and Medium size products");
+            //Console.WriteLine("Green and Medium size products");
 
-            foreach (var prod in bf.Filter(productList,
-                new AndSpecification(new ColorSpecification(Color.GREEN),new SizeSpecification(Size.MEDIUM))))
-            {
-                Console.WriteLine($"{prod.Name} is green and medium");
-            }//end of foreach 
+            //foreach (var prod in bf.Filter(productList,
+            //    new AndSpecification(new ColorSpecification(Color.GREEN),new SizeSpecification(Size.MEDIUM))))
+            //{
+            //    Console.WriteLine($"{prod.Name} is green and medium");
+            //}//end of foreach 
+            #endregion
+
+            #region LSP
+            Rectangle rec = new Rectangle(2, 3);
+            Console.WriteLine($"{rec} has area {GetArea(rec)}");
+
+            Rectangle sq = new Square();
+            sq.Width = 4;
+            Console.WriteLine($"{sq} has area {GetArea(sq)}");
             #endregion
         }
     }//end of class Program

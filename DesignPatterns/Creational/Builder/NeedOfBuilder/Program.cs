cb21a0f758f58fdd4c414949e50f1ab535b6e94a
Satej More <satejmore@gmail.com>
2018-03-23 20:30:08 +0530

    -Added the solution for need of builder for creating objects.

---
+++ DesignPatterns/Creational/Builder/NeedOfBuilder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedOfBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Hello";
+            string[] orderList = {"Order1","Order2","Order3"};
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append("<p>");
+            builder.Append(name);
+            builder.Append("</p>");
+
+            Console.WriteLine(builder);
+
+            builder.Clear();
+
+            builder.Append("<ul>");
+            foreach (var order in orderList)
+            {
+                builder.Append($"<li>{order}</li>");
+            }
+            builder.Append("</ul>");
+            Console.WriteLine(builder);
+
+        }
+    }//end of class Program
+}//end of namespace NeedOfBuilder

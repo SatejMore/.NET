43919822c305c1984a3316ef2bd126d8b5cead5e
Satej More <satejmore@gmail.com>
2019-01-06 22:19:33 +0530

    -Added vidly ASP.NET MVC5 project.

---
+++ ASP.NETMVC5/Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            Movie movieObject = new Movie { Name = "Drishyam", Id = 1 };
+            return View(movieObject);
+        }
+    }
+}

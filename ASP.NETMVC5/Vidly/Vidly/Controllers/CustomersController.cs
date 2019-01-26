75b1f770ac8bad278a2fdea79fb394506e21b18d
Satej More <satejmore@gmail.com>
2019-01-26 22:50:10 +0530

    -Added solution for section 2 problem statement.

---
+++ ASP.NETMVC5/Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers.ToList());
+        }
+
+        public ActionResult CustomerDetails(int id)
+        {
+            var customers = GetCustomers().ToList();
+            if (customers.Exists(c => c.Id == id))
+            {
+                return View(customers[id - 1]);
+            }
+            return HttpNotFound();
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer {Id = 1, Name="Satej" },
+                new Customer {Id = 2, Name="Temp" }
+            };
+            return customers;
+        }
+
+    }
+}
--- ASP.NETMVC5/Vidly/Vidly/Controllers/MoviesController.cs
+++ ASP.NETMVC5/Vidly/Vidly/Controllers/MoviesController.cs
@@ -4,16 +4,48 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movie
-        public ActionResult Random()
+        public ActionResult Index()
         {
-            Movie movieObject = new Movie { Name = "Drishyam", Id = 1 };
-            return View(movieObject);
+            var movies = GetMovies();
+            return View(movies);
         }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            var movies = new List<Movie>{
+                new Movie { Id =1 , Name="Shreak"},
+                new Movie {Id=2 , Name="Wall-E" }
+            };
+            return movies;
+        }
+
+
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content(string.Format("Id={0}", id));
+        //}
+
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+        //    return Content(string.Format("PageIndex={0} , SortBy={1}", pageIndex, sortBy));
+        //}
+
+        //[Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        //public ActionResult ReleaseByMonth(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
+
     }
 }
